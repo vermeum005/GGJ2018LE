0c6f292e8d730321b62b7ef3d88729669039dc1b
mji34 <mauro_vermeulen@hotmail.com>
2018-01-26 21:09:24 +0100

    cow added

--- Cattlepult/Assets/Scripts/Cow behaviour.cs
+++ Cattlepult/Assets/Scripts/Cow behaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Cowbehaviour : MonoBehaviour {
-    enum State { Idle, BreedingIdle, Running, PickUp, CattlePult};
+    enum State { Idle, BreedingIdle, Running, PickUp, CattlePult, Breeding};
     // Use this for initialization
     private State state;
 	void Start () {
@@ -28,6 +28,9 @@ public class Cowbehaviour : MonoBehaviour {
                 break;
 
             case State.CattlePult:
+                break;
+
+            case State.Breeding:
                 break;
         }
 	}
