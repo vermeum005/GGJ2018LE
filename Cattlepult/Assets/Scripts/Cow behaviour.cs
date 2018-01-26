bdbda0595ab71a90d2dd159b85544bf0c1dfba7a
mji34 <mauro_vermeulen@hotmail.com>
2018-01-26 21:01:22 +0100

    begin

---
+++ Cattlepult/Assets/Scripts/Cow behaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cowbehaviour : MonoBehaviour {
+    enum State { Idle, BreedingIdle, Running, PickUp, CattlePult};
+    // Use this for initialization
+    private State state;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+		switch (state)
+        {
+            case State.Idle:
+                break;
+
+            case State.BreedingIdle:
+                break;
+
+            case State.Running:
+                break;
+
+            case State.PickUp:
+                break;
+
+            case State.CattlePult:
+                break;
+        }
+	}
+}
